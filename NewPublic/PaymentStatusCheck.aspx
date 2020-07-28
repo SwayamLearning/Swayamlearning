18040b65cc46e57872b8b84d427665ee42008581
narendraom10 <68589795+narendraom10@users.noreply.github.com>
2020-07-28 15:50:19 +0530

    Payment Status CHeck

---
+++ NewPublic/PaymentStatusCheck.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PaymentStatusCheck.aspx.cs" Inherits="NewPublic_PaymentStatusCheck" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblOrderidTitle" runat="server" Text="Order ID : "></asp:Label><asp:TextBox ID="txtOrderID" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnCheckStatus" runat="server" Text="Check Status" OnClick="btnCheckStatus_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ NewPublic/PaymentStatusCheck.aspx.cs
@@ -0,0 +1,51 @@
+﻿using CCA.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class NewPublic_PaymentStatusCheck : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnCheckStatus_Click(object sender, EventArgs e)
+    {
+        string merchant_id = "264897"; //"99522";
+        string access_code = "AVJH93HG41AV94HJVA"; //"AVFW65DE39BV30WFVB";
+        string Working_key = "198C5C10C9BC8B5FEB23250E347F7A4C"; //"E85FE1783919FA34A4758580E844135A";
+        object EncReq = new
+        {
+            reference_no = "",
+            order_no = txtOrderID.Text.Trim(),
+            
+        };
+        string ccaRequest = (new JavaScriptSerializer()).Serialize(EncReq);
+        CCACrypto ccaCrypto = new CCACrypto();
+        string strEncRequest = ccaCrypto.Encrypt(ccaRequest, Working_key);
+        string apiUrl = "https://api.ccavenue.com/apis/servlet/DoWebTrans";
+        object input = new
+        {
+            enc_request = strEncRequest,
+            access_code = access_code,
+            command = "orderStatusTracker",
+            request_type = "JSON",
+            version = "1.1"
+
+        };
+        string inputJson = (new JavaScriptSerializer()).Serialize(input);
+        WebClient client = new WebClient();
+        client.Headers["Content-type"] = "application/json";
+        client.Encoding = Encoding.UTF8;
+        ServicePointManager.Expect100Continue = true;
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        string json = client.UploadString(apiUrl, inputJson);
+
+    }
+}
